b0f8786416dc1a54dd4f27a09756e16e0916086e
Narong Phath <narongphath@gmail.com>
2016-02-06 09:33:35 -0500

    Edit gemfile with heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,17 @@
 class PostsController < ApplicationController
 	before_action :find_params, only: [:show, :edit, :update, :destroy]
 	def index
-		@posts = Post.all.order("created_at DESC")
+		@posts = Post.all.limit(3).order("created_at DESC")
 	end
 	def new
 		@post = Post.new
 	end
 	def create
-		@post = Post.new(post_params)
+		@post = Post.new post_params
 		if @post.save
-			redirect_to root_url
+			redirect_to root_url, notice: "Your post is successfully saved."
 		else
-			render 'new'
+			render 'new', notice: "Your post willnot be saved!!"
 		end
 	end
 	def show
